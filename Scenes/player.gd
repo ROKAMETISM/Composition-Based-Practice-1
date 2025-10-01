eb8d607536f01fac62c53d63745ab2c4a1ee12fb
ROJS <mskim829@naver.com>
2025-10-01 18:52:51 +0900

    Implementing Player

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -6,3 +6,4 @@ func _process(delta: float) -> void:
 	var input_vector = Vector2(int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left")), 
 								int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up")))
 	velocity = input_vector.normalized() * speed
+	move_and_slide()
