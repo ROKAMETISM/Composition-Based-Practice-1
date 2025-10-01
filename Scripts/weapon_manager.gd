04fa1985ae83535394b29f298af1c5fffbc16cc8
ROJS <mskim829@naver.com>
2025-10-01 23:45:24 +0900

    Implement Searching Closest Enemy And Autoaim

--- Scripts/weapon_manager.gd
+++ Scripts/weapon_manager.gd
@@ -10,5 +10,23 @@ func _ready() -> void:
 		weapon.player = get_parent()
 	
 func _process(delta: float) -> void:
+	var closest_enemy = get_closest_enemy()
 	for weapon in weapons:
-		weapon.update(delta)
+		weapon.update(delta, closest_enemy)
+
+func get_closest_enemy() -> CharacterBody2D:
+	var closest_enemy: CharacterBody2D = null
+	var closest_distance := INF
+	var player = get_parent()
+
+	# Find all CharacterBody2D nodes
+	for character in get_tree().get_nodes_in_group("Enemies"):
+		if character == player:
+			continue  # skip self
+
+		var dist = player.global_position.distance_to(character.global_position)
+		if dist < closest_distance:
+			closest_distance = dist
+			closest_enemy = character
+
+	return closest_enemy
--- Scripts/weapon_rifle.gd
+++ Scripts/weapon_rifle.gd
@@ -8,16 +8,19 @@ var rifle_projectile = preload("res://Scenes/projectile_rifle.tscn")
 var tree_current_scene
 var player
 
-func update(delta : float) -> void:
+func update(delta : float, closest_enemy) -> void:
 	fire_timer -= delta
 	if fire_timer < 0 :
 		fire_timer += fire_interval
-		fire_weapon()
+		fire_weapon(closest_enemy)
 
-func fire_weapon() -> void:
+func fire_weapon(closest_enemy) -> void:
 	print("weapon fire called")
 	var projectile = rifle_projectile.instantiate()
 	projectile.global_position = player.global_position
-	projectile.rotation = player.rotation
+	if closest_enemy:
+		projectile.rotation = (closest_enemy.global_position - player.global_position).angle()
+	else:
+		projectile.rotation = 0.0
 	tree_current_scene.add_child(projectile)
 	#Todo : Implement auto-aim at closest enemy
