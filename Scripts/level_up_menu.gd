c7ce7bffc5802df883e95d0d2504cc8d075a18fd
ROJS <mskim829@naver.com>
2025-10-05 22:08:45 +0900

    Update level_up_menu.gd

--- Scripts/level_up_menu.gd
+++ Scripts/level_up_menu.gd
@@ -1,1 +1,8 @@
 extends Control
+
+@export var upgrade_card_scene = preload("uid://cyorv468553vi")
+
+@onready var card_container = $HBoxContainer
+
+func _ready():
+	visible = false  # hidden by default until level-up happens
