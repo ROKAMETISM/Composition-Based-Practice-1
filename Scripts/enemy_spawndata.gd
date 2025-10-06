91ddf5c39ba854f28f2b2fb3354778129274595e
ROJS <mskim829@naver.com>
2025-10-06 15:09:31 +0900

    Update enemy_spawndata.gd

--- Scripts/enemy_spawndata.gd
+++ Scripts/enemy_spawndata.gd
@@ -7,10 +7,10 @@ const ENEMY_SLOW = preload("res://Scenes/enemy_slow.tscn")
 const ENEMY_FAST = preload("res://Scenes/enemy_fast.tscn")
 
 func _ready() -> void:
-	enemy_spawn_intervals[ENEMY_SLOW] = 1.0
+	enemy_spawn_intervals[ENEMY_SLOW] = 0.5
 	enemy_spawn_distance_range[ENEMY_SLOW] = Vector2(200, 400)
 	
-	enemy_spawn_intervals[ENEMY_FAST] = 1.3
+	enemy_spawn_intervals[ENEMY_FAST] = 0.76
 	enemy_spawn_distance_range[ENEMY_FAST] = Vector2(300, 600)
 	
 
