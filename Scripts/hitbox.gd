fccee2a328465972c2b27373d351b3988ac77fa8
ROJS <mskim829@naver.com>
2025-10-06 14:48:26 +0900

    Implement Enemy hitting player with damage

--- Scripts/hitbox.gd
+++ Scripts/hitbox.gd
@@ -1,1 +1,14 @@
 extends Area2D
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	#detected a Player hit by this Enemy's Hitbox
+	if body == null:
+		return
+	if body != get_parent().player:
+		return
+	var hpcomponent = body.get_node_or_null("HPComponent")
+	if hpcomponent:
+		hpcomponent.take_damage(get_parent().enemy_damage)
+		get_parent().queue_free()
