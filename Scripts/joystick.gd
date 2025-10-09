285a0869d6935d3f799409da2ab4b19edc054c62
ROJS <mskim829@naver.com>
2025-10-09 09:58:19 +0900

    Edit Joystick Base button

--- Scripts/joystick.gd
+++ Scripts/joystick.gd
@@ -2,3 +2,12 @@ extends Control
 @onready var base = $Base
 @onready var knob = $Knob
 var pressing := false
+var relative_pointer_position := Vector2.ZERO
+func _process(delta: float) -> void:
+	if pressing:
+		relative_pointer_position = get_global_mouse_position() - global_position
+		
+func _on_base_button_down() -> void:
+	pressing = true
+func _on_base_button_up() -> void:
+	pressing = false
