107eb268b3f81f8da9338dd634c9dfb124f62cda
ROJS <mskim829@naver.com>
2025-10-09 09:48:49 +0900

    Update UpgradeCard text

--- Scripts/upgrade_card.gd
+++ Scripts/upgrade_card.gd
@@ -4,4 +4,4 @@ var index : int
 
 func set_upgrade_data(new_index, upgrade_data) -> void:
 	index = new_index
-	text = upgrade_data[0].weapon_name + " - " + upgrade_data[1]
+	text = upgrade_data[0].weapon_name + "\n" + upgrade_data[1]
